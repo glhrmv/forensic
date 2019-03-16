038045b565ecf58f5f072501fe767e4ac80bf5d9
glhrmv <gvmgui@gmail.com>
2019-03-16 15:01:08 +0000

    Finish parsing all possible command line options

--- src/forensic.c
+++ src/forensic.c
@@ -1,15 +1,35 @@
+#include "forensic.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
+#include <regex.h>
 
 int main(int argc, char** argv) {
-  /* Flags */
-  int debug_flag = 0;
-  int r_flag = 0, h_flag = 0, o_flag = 0, v_flag = 0;
-  char* h_name; char* o_name;
+  /* Declare and initialize program config */
+  ProgramConfig program_config = empty_program_config;
+
+  /* Modify program_config according to argv */
+  parse_args(argc, argv, &program_config);
+
+  /* Begin processing */
+  process(program_config);
+
+  exit(0);
+}
+
+int file_exists(const char* pathname) {
+  return access(pathname, F_OK);
+}
+
+void parse_args(int argc, char** argv, ProgramConfig* program_config) {
+  /* Program usage */
+  char* usage = "usage: %s [-r] [-h [md5[,sha1[,sha256]]]] [-o <outfile>] [-v] <file|dir>\n";
 
   /* Using getopt to parse args */
-  static char* usage = "usage: %s [-r] [-h [md5[,sha1[,sha256]]]] [-o <outfile>] [-v] <file|dir>\n";
   extern char* optarg;
   extern int optind;
   int c, err = 0;
@@ -17,21 +37,34 @@ int main(int argc, char** argv) {
   while ((c = getopt(argc, argv, "drh:o:v")) != -1) {
     switch (c) {
       case 'd':
-        debug_flag = 1;
+        program_config->debug_flag = 1;
         break;
       case 'r':
-        r_flag = 1;
+        program_config->r_flag = 1;
         break;
       case 'h':
-        h_flag = 1;
-        h_name = optarg;
+        if (program_config->h_flag)
+          fprintf(stdout, "(warning) -h is set multiple times\n");
+
+        program_config->h_flag = 1;
+        program_config->h_value = optarg;
         break;
       case 'o':
-        o_flag = 1;
-        o_name = optarg;
+        if (program_config->o_flag)
+          fprintf(stdout, "(warning) -o is set multiple times\n");
+
+        program_config->o_flag = 1;
+        program_config->o_value = optarg;
         break;
       case 'v':
-        v_flag = 1;
+        program_config->v_flag = 1;
+
+        if (getenv("LOGFILENAME")) {
+          program_config->logfile = getenv("LOGFILENAME");
+        } else {
+          printf("LOGFILENAME env variable not set, using 'log.txt' by default\n");
+          program_config->logfile = "log.txt";
+        }
         break;
       case '?':
         err = 1;
@@ -39,30 +72,85 @@ int main(int argc, char** argv) {
     }
   }
 
-  // In case of invalid option
+  /* If invalid option */
   if (err) {
     fprintf(stderr, usage, argv[0]);
     exit(1);
   }
 
-  // Print options
-  if (debug_flag) {
-    printf("r_flag = %d\n", r_flag); 
-    printf("h_flag = %d\n", h_flag); 
-    printf("h_name = \"%s\"\n", h_name); 
-    printf("o_flag = %d\n", o_flag); 
-    printf("o_name = \"%s\"\n", o_name);
-    printf("v_flag = %d\n", v_flag); 
-  }
-
-  /* If no args */
+  /* If no arg (no file/dir specified) */
   if (optind == argc) {
     fprintf(stderr, usage, argv[0]);
     exit(1);
   } 
-  
-  // Argument passed
-  fprintf(stdout, "file or folder: %s\n", argv[optind]);
 
-  exit(0);
+  /* If h option set */
+  if (program_config->h_flag) {
+    /* Parse the comma-delimited string */
+    char* str_to_parse = malloc(strlen(program_config->h_value) + 1);
+    strcpy(str_to_parse, program_config->h_value);
+
+    char* token;
+    while ((token = strsep(&str_to_parse, ","))) {
+      if (strcmp(token, "md5") == 0) {
+        program_config->h_alg_md5_flag = 1;
+        continue;
+      }
+
+      if (strcmp(token, "sha1") == 0) {
+        program_config->h_alg_sha1_flag = 1;
+        continue;
+      }
+
+      if (strcmp(token, "sha256") == 0) {
+        program_config->h_alg_sha256_flag = 1;
+        continue;
+      }
+
+      fprintf(stderr, "(warning) -h : invalid hash algorithm sent: \"%s\" (ignored)\n", token);
+    }
+
+    free(str_to_parse);
+
+    /* If none of the valid algorithms were input */
+    if (!program_config->h_alg_md5_flag && !program_config->h_alg_sha1_flag && !program_config->h_alg_sha256_flag) {
+      fprintf(stderr, "-h : no valid option found\n");
+      fprintf(stderr, usage, argv[0]);
+      exit(1);
+    }
+  }
+
+  /* If o option set */
+  if (program_config->o_flag) {
+    /* Check if given file already exists */
+    if (file_exists(program_config->o_value) == 0) {
+      fprintf(stderr, "-o %s : file already exists\n", program_config->o_value);
+      exit(1);
+    }
+  }
+
+  /* All good: first argument after options is what we want */
+  program_config->arg = argv[optind];
+
+  /* Print options (debug mode) */
+  if (program_config->debug_flag) {
+    printf("debug_flag = %d\n", program_config->debug_flag); 
+    printf("r_flag = %d\n", program_config->r_flag); 
+    printf("h_flag = %d\n", program_config->h_flag); 
+    printf("h_value = \"%s\"\n", program_config->h_value); 
+    printf("h_alg_md5_flag = %d\n", program_config->h_alg_md5_flag);
+    printf("h_alg_sha1_flag = %d\n", program_config->h_alg_sha1_flag);
+    printf("h_alg_sha256_flag = %d\n", program_config->h_alg_sha256_flag);
+    printf("o_flag = %d\n", program_config->o_flag); 
+    printf("o_value = \"%s\"\n", program_config->o_value);
+    printf("v_flag = %d\n", program_config->v_flag); 
+    printf("logfile = \"%s\"\n", program_config->logfile); 
+    printf("arg value = \"%s\"\n", program_config->arg); 
+  }
+}
+
+void process(const ProgramConfig program_config) {
+  fprintf(stdout, "file or folder: %s\n", program_config.arg);
+
+  /* TOOD: Begin processing argument passed */
 }
---
+++ src/forensic.h
@@ -0,0 +1,56 @@
+#ifndef FORENSIC_H
+#define FORENSIC_H
+
+/**
+ * @brief A structure to represent the program's configuration.
+ */
+typedef struct ProgramConfig {
+  int debug_flag; // Debug flag
+  int r_flag; // Recursion flag (for directories)
+  int h_flag; // Hash flag
+  char* h_value; // Hash option value (comma-delimited string)
+  int h_alg_md5_flag; // MD5 hash algorithm flag
+  int h_alg_sha1_flag; // SHA1 hash algorithm flag
+  int h_alg_sha256_flag; // SHA256 hash algorithm flag
+  int o_flag; // Output file flag
+  char* o_value; // Output file option value
+  int v_flag; // Verbose flag (to use logfile)
+  char* logfile; // Logfile to be used
+  char* arg; // Argument passed to program (file or directory)
+} ProgramConfig;
+
+/**
+ * Declare a const static instance of ProgramConfig
+ * which will have all of its fields initialized due to static initialization.
+ * See: https://stackoverflow.com/a/6891731/6304441
+ */
+static const ProgramConfig empty_program_config;
+
+/**
+ * @brief Check if file exists on disk.
+ * 
+ * @param pathname Path name
+ * @return Zero on success, non-zero otherwise
+ */
+int file_exists(const char* pathname);
+
+/**
+ * Parses arguments (using getopt) and fills a ProgramConfig struct.
+ * If any of the parameters are invalid/unexpected, prints error message(s)
+ * to stderr and exits with status code 1.
+ * 
+ * @param argc Argument count (from main())
+ * @param argv Argument vector (from main())
+ * @param program_config ProgramConfig struct to fill
+ */
+void parse_args(int argc, char** argv, ProgramConfig* program_config);
+
+/**
+ * @brief Main program logic function.
+ * 
+ * @param program_config ProgramConfig struct
+ */
+void process(const ProgramConfig program_config);
+
+#endif
+
